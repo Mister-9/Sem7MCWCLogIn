b5cba7af5bee7567a4735efa521e3d92cd3e6473
Mister-9 <bhavin9jan@gmail.com>
2017-08-29 17:11:32 +0530

    Final Commit

--- app/src/main/java/com/mr9/login/LogInActivity.java
+++ app/src/main/java/com/mr9/login/LogInActivity.java
@@ -3,14 +3,12 @@ package com.mr9.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
-    private static EditText out;
+    private static TextView out;
 
     private static Button retry;
 
@@ -23,11 +21,12 @@ public class LogInActivity extends AppCompatActivity {
         String pass_=intent.getExtras().getString("pass");
         retry=(Button)findViewById(R.id.try_again);
         String st;
-        out = (EditText) findViewById(R.id.output);
-        Toast.makeText(this,u_name,Toast.LENGTH_LONG).show();
-        Log.d("bhavin123","username:"+u_name+"pass:"+pass_);
+        out = (TextView) findViewById(R.id.output);
         if((u_name.compareTo("mister_9") + pass_.compareTo("bhavink"))==0) {
             st = "Log In Successful !!!\nWelcome "+u_name;
+        }
+        else if(pass_.compareTo("bhavink")!=0 && u_name.compareTo("mister_9")!=0){
+            st = "Sorry!!\nWrong Username and Password!!";
         }
         else if(pass_.compareTo("bhavink")!=0) {
             st = "Sorry!!\nWrong Password!!";
@@ -36,8 +35,8 @@ public class LogInActivity extends AppCompatActivity {
             st = "Sorry!!\nWrong Username!!";
         }
         else {
-                st = "Sorry!!\nWrong Username and Password!!";
-            }
+            st="Sorry!!\nTry Again !";
+        }
         out.setText(st);
         retry.setOnClickListener(
                 new View.OnClickListener() {
